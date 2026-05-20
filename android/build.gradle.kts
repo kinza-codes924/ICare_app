f071a7e8f414e0531af3a816d2d1986c2566b665
Wajahat <wajahat@icare.com>
2026-05-21 00:06:30 +0500

    fix: remove legacy buildscript block, revert AGP to 8.11.1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,13 +1,3 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
-    }
-}
-
 allprojects {
     repositories {
         google()
